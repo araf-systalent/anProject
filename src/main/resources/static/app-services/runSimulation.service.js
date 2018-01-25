273ef7903852d84ac2139c86732b9e773b053a64
SYSTALENT\arafullahi <arafullahi@systalent.com>
2018-01-25 16:29:24 +0530

    URI-19_Run_Simulation_Integration Simulation run and get specific result set simulation api integrated

--- src/main/resources/static/app-services/runSimulation.service.js
+++ src/main/resources/static/app-services/runSimulation.service.js
@@ -23,6 +23,8 @@
     function RunSimulationService($http,$rootScope) {
         var service = {};
         service.getDropDownData=runSimulationDropdownData;
+        service.runSimulation=runSimulation;
+        service.getSpecificresult=simulationResultForSpecific;
         return service;
         function runSimulationDropdownData(){
             var apiBaseUrl=$rootScope.ApiBaseUrl;            
@@ -30,14 +32,39 @@
            
         } 
 
+        function runSimulation(data){
+            var apiBaseUrl=$rootScope.ApiBaseUrl; 
+            //$http.get(apiBaseUrl+'api/dropdowns/simulation').then(handleSuccess, handleError('Error getting runsimulation dropdown data'));
+            return $http({
+                url : apiBaseUrl+'api/simulation/run',
+                method : 'POST',
+                "content-type": "application/json;",
+                data:data,
+                transformResponse: [function (data) {
+                    // Do whatever you want!
+                    return {data:data};
+                }]
+              }).then(handleSuccess, handleError('Error getting runsimulation '));
+        }
+
+        function simulationResultForSpecific(data){
+            var apiBaseUrl=$rootScope.ApiBaseUrl; 
+            //$http.get(apiBaseUrl+'api/dropdowns/simulation').then(handleSuccess, handleError('Error getting runsimulation dropdown data'));
+            return $http({
+                url : apiBaseUrl+'api/simulation',
+                method : 'POST',
+                "content-type": "application/json;",
+                data:data
+              }).then(handleSuccess, handleError('Error getting simulation results for specific'));
+        }
+
+
         function handleSuccess(res) {
-            return   res.data ;
+            return   {success:true,data:res.data} ;
         }
 
         function handleError(error) {
-            return function () {
-                return { success: false, message: error };
-            };
+            return  { success: false, message: error };
         }
        
 
--- src/main/resources/static/app/controllers/runsimulation.controller.js
+++ src/main/resources/static/app/controllers/runsimulation.controller.js
@@ -8,10 +8,18 @@
         RunSimulation.$inject = ['UserService', '$rootScope','$scope','$http','RunSimulationService','dropDownresponse'];
     function RunSimulation(UserService, $rootScope,$scope,$http,RunSimulationService,dropDownresponse) {
         var vm = this;
-        $rootScope.catClass=[];
-        $rootScope.clientGroup=[];
-        $rootScope.priceStructure=[];
+        vm.catClasses=[];
+        vm.clientGroups=[];
+        vm.priceStructures=[];
         $rootScope.pageHeader="Run simulation";
+        vm.simRunData={}
+        vm.statistics={
+            estimatePrftChg:"-",
+            stdDevPrftChg:"-",
+            avgChgRentalLength:"-",
+            rentalAnalyzed:"-",
+            daysAnalyzed:"-"
+        };
         initController();
        
         function initController() {
@@ -26,19 +34,115 @@
             
         }
 
+        vm.executeSimulationRun=executeSimulationRun;
+
         function loadDropDownData(){
 
             setDropdownElementDatas(dropDownresponse);
            
             
         }
+        function executeSimulationRun(){
+            console.log(vm.simRunData.fromDate);
+            var simParam =generateSimParam()
+            RunSimulationService.runSimulation(simParam).then(function (response) {
+               if(response.success){
+                   
+                   populateChartAndStatisticsBasedOnResult(response.data.data);
+
+
+               }
+               else{
+                   return {};
+               }
+             });
+        }
+
+        function generateSimParam(){
+            var simParam={
+                "priceScheme": {
+                  "type": vm.simRunData.priceStructure,
+                  "level": 1
+                },
+                "grouping": {
+                  "clientGroup": vm.simRunData.clientGroup,
+                  //"rateZone": "rate zone 1",
+                  "catClass": vm.simRunData.catClass
+                },
+                "simType": {
+                  "nRun": 1000,
+                  "sampleType": "HISTORIC",
+                //   "dataStart":vm.simRunData.fromDate.toISOString().slice(0,-1),
+                //   "dataEnd": vm.simRunData.toDate.toISOString().slice(0,-1),
+                  "dataStart": "2018-01-11T15:25:41.390",
+                  "dataEnd": "2018-04-21T15:25:41.398"
+                },
+                "startDate": vm.simRunData.fromDate.toISOString().slice(0,-1),
+                "endDate": vm.simRunData.toDate.toISOString().slice(0,-1),
+                "rentalLengthChange":null
+              };
+              return simParam;
+        }
+
+        function populateChartAndStatisticsBasedOnResult(simId){
+            getSimulationResutById(simId).then(function (response) {
+                if(response.success){
+                    var simulationresult= response.data;
+                    var simParamClientGroup = simulationresult.params.grouping.clientGroup;
+                    var simParamCatClass = simulationresult.params.grouping.catClass;
+                    populateSatisticsData(simulationresult.stats[simParamClientGroup]);
+                    populateCumulativeRevenueChartData(simulationresult.cumulativeCharts[simParamClientGroup][simParamCatClass].REVENUE);
+                }
+                else{
+                    return {};
+                }
+              });
+        }
+        
+        function populateCumulativeRevenueChart(cumulativeRevenueChartsData){
+            console.log(cumulativeRevenueChartsData);
+        }
+        function populateSatisticsData(statistics){
+            
+            vm.statistics={
+                estimatePrftChg:statistics.PROFIT_CHANGE.avg,
+                stdDevPrftChg:statistics.PROFIT_CHANGE.sd,
+                avgChgRentalLength:"-",
+                rentalAnalyzed:"-",
+                daysAnalyzed:"-"
+            };
+        }
+        function getSimulationResutById(simId){
+            var simIdArr = [];
+            simIdArr.push(parseFloat(simId));
+            return getSpecificSimulationResult(simIdArr).then(function (response) {
+                if(response.success){
+                    return {success:true,data:response.data[0]};
+                }
+                else{
+                    return response;
+                }
+              });
+        }
+
+        function getSpecificSimulationResult(simIdArr){
+            console.log(simIdArr);
+            return RunSimulationService.getSpecificresult(simIdArr).then(function (response) {
+                if(response.success){
+                    return response;
+                }
+                else{
+                    return response;
+                }
+              });
+        }
 
         function setDropdownElementDatas(data){
-            vm.catClass=data["Price Structure"].values;
-            vm.catClass =unique(vm.catClass);
-            vm.clientGroup=data["Client Group"].values;
-            vm.clientGroup =unique(vm.clientGroup);
-            vm.priceStructure=data["Price Structure"].values;
+            vm.catClasses=data["Cat Class"].values;
+            vm.catClasses =unique(vm.catClasses);
+            vm.clientGroups=data["Client Group"].values;
+            vm.clientGroups =unique(vm.clientGroups);
+            vm.priceStructures=data["Price Structure"].values;
         }
 
         function unique(list) {
